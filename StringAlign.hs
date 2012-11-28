b78e7e6206dc69b2761392e72b2f161768fe84fb
Tommy <tommy.kvant@gmail.com>
2012-11-28 23:18:16 +0100

    Nästan klar optimering
    Får konstiga fel vid kompilering

--- StringAlign.hs
+++ StringAlign.hs
@@ -20,8 +20,8 @@ similarityScore xs ys = similarity (length xs) (length ys)
 		simEntry :: Int -> Int -> Int
 		simEntry 0 0 = 0
 		simEntry i j
-			|j == 0 = simEntry (i-1) 0 + matchScore x '-'
-			|i == 0 = simEntry 0 (j-1) + matchScore '-' y
+			|j == 0 = simEntry (i-1) j + matchScore x '-'
+			|i == 0 = simEntry i (j-1) + matchScore '-' y
 			|otherwise = maximum [(simEntry (i-1) (j-1) + matchScore x y), (simEntry (i-1) j + matchScore x '-'),(simEntry i (j-1) + matchScore '-' y)]
 			where
 				x = xs!!(i-1)
@@ -39,33 +39,16 @@ matchScore x y
 attachHeads :: a -> a -> [([a],[a])] -> [([a],[a])]
 attachHeads h1 h2 aList = [(h1:xs,h2:ys) | (xs,ys) <- aList]
 
-attachTails :: a -> a -> [([a],[a])] -> [([a],[a])]
-attachTails t1 t2 aList = [(xs++[t1],ys++[t2]) | (xs,ys) <- aList]
-
 ----- 2c -----
 maximaBy :: Ord b => (a -> b) -> [a] -> [a]
 maximaBy valueFcn xs = [x | x <- xs, valueFcn x == maximum (map valueFcn xs)]
 
 ----- 2d -----
-optAlignments :: String -> String -> [AlignmentType]
-{-optAlignments [] [] = [("","")]
+{-optAlignments :: String -> String -> [AlignmentType]
+optAlignments [] [] = [("","")]
 optAlignments (x:xs) [] = attachHeads x '-' (optAlignments xs [])
 optAlignments [] (y:ys) = attachHeads '-' y (optAlignments [] ys)
-optAlignments (x:xs) (y:ys) = concat (maximaBy (optMatchScore.head) [(attachHeads x y (optAlignments xs ys)), (attachHeads x '-' (optAlignments xs (y:ys))), (attachHeads '-' y (optAlignments (x:xs) ys))])-}
-optAlignments xs ys = optAlign (length xs) (length ys)
-	where
-		optAlign i j = optTable!!i!!j
-		optTable = [[ optEntry i j | j<-[0..]] | i<-[0..] ]
-		
-		optEntry :: Int -> Int -> [AlignmentType]
-		optEntry 0 0 = [("","")]
-		optEntry i j
-			|j == 0 = attachTails x '-' (optEntry (i-1) 0)
-			|i == 0 = attachTails '-' y (optEntry 0 (j-1))
-			|otherwise = concat (maximaBy (optMatchScore.head) [(attachTails x y (optEntry (i-1) (j-1))), (attachTails x '-' (optEntry (i-1) j)), (attachTails '-' y (optEntry i (j-1)))])
-			where
-				x = xs!!(i-1)
-				y = ys!!(j-1)
+optAlignments (x:xs) (y:ys) = concat (maximaBy (optMatchScore.head) [(attachHeads x y (optAlignments xs ys)), (attachHeads x '-' (optAlignments xs (y:ys))), (attachHeads '-' y (optAlignments (x:xs) ys))])
 
 optMatchScore :: AlignmentType -> Int
 optMatchScore (string1, string2) = optScorer string1 string2
@@ -73,13 +56,42 @@ optMatchScore (string1, string2) = optScorer string1 string2
 optScorer :: String -> String -> Int
 optScorer [] _ = 0
 optScorer (x:xs) (y:ys) = (matchScore x y) + (optScorer xs ys)
+-}
+
+optAlignmentsOpt :: String -> String -> [AlignmentType]
+optAlignmentsOpt xs ys = optAlign (length xs) (length ys)
+	where
+		optAlign i j = snd (optTable!!i!!j)
+		optTable = [[ optEntry i j | j<-[0..]] | i<-[0..] ]
+		
+		optEntry :: Int -> Int -> (Int, [AlignmentType])
+		optEntry 0 0 = (0, [("","")])
+		optEntry i j
+			|j == 0 = createOptEntry x '-' (optEntry (i-1) j)
+			|i == 0 = createOptEntry '-' y (optEntry i (j-1))
+			|otherwise = concatOptEntryList (maximaBy (fst.head) [createOptEntry x y (optEntry (i-1) (j-1)), createOptEntry x '-' (optEntry (i-1) j), createOptEntry '-' y (optEntry i (j-1))])
+			where
+				x = xs!!(i-1)
+				y = ys!!(j-1)
+
+createOptEntry :: Char -> Char -> (Int, [AlignmentType]) -> (Int, [AlignmentType])
+createOptEntry x y prevEntry = ((fst prevEntry)+ matchScore x y, attachTails x y (snd prevEntry))
+
+concatOptEntryList :: [(Int, [AlignmentType])] -> (Int, [AlignmentType])
+concatOptEntryList list = (head(fst unz),concat(snd unz))
+	where unz = unzip list
+
+attachTails :: a -> a -> [([a],[a])] -> [([a],[a])]
+attachTails t1 t2 aList = [(xs++[t1],ys++[t2]) | (xs,ys) <- aList]
+
+
 
 ----- 2e -----
 outputOptAlignments :: String -> String -> IO ()
 outputOptAlignments string1 string2 = do
 							putStrLn ("There are " ++ show (length aligns) ++ " optimal alignments")
 							printAligns aligns
-							where 	aligns = (optAlignments string1 string2)
+							where 	aligns = (optAlignmentsOpt string1 string2)
 									
 											
 printAligns :: [AlignmentType] -> IO()
