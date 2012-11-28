62f4b49f65cf27451ead9c78e87edec9d3687961
Tommy <tommy.kvant@gmail.com>
2012-11-28 17:09:53 +0100

    Ändrat i basfall
    För optAlignments och SimilarityScore

--- StringAlign.hs
+++ StringAlign.hs
@@ -6,10 +6,24 @@ scoreSpace = -1
 
 type AlignmentType = (String, String)
 
+----- 2a -----
 similarityScore :: String -> String -> Int
-similarityScore x [] = (length x) * scoreSpace
-similarityScore [] y = (length y) * scoreSpace
-similarityScore (x:xs) (y:ys) = maximum [(similarityScore xs ys + matchScore x y), (similarityScore xs (y:ys) + matchScore x '-'),(similarityScore (x:xs) ys + matchScore '-' y)]
+similarityScore [] [] = 0
+similarityScore (x:xs) [] = similarityScore xs [] + matchScore x '-'
+similarityScore [] (y:ys) = similarityScore [] ys + matchScore '-' y
+similarityScore (x:xs) (y:ys) = maximum [(similarityScore xs ys + matchScore x y), (similarityScore xs (y:ys) + matchScore x '-'),(similarityScore (x:xs) ys + matchScore '-' y)]{-
+similarityScore xs ys = similarity (length xs) (length ys)
+	where
+		similarity i j = simTable!!i!!j
+		simTable = [[ simEntry i j | j<-[0..]] | i<-[0..] ]
+	
+		simEntry :: Int -> Int -> Int
+		simEntry x [] = (length x) * scoreSpace
+		simEntry [] y = (length y) * scoreSpace
+		simEntry (x:xs) (y:ys) = maximum [(similarityScore xs ys + matchScore x y), (similarityScore xs (y:ys) + matchScore x '-'),(similarityScore (x:xs) ys + matchScore '-' y)]
+		where
+			x = xs!!(i-1)
+			y = ys!!(j-1)-}
 
 matchScore :: Char -> Char -> Int
 matchScore _ '-' = scoreSpace
@@ -18,16 +32,20 @@ matchScore x y
 			|x == y = scoreMatch
 			|otherwise = scoreMismatch
 
+----- 2b -----
 -- Lägger till h1 och h2 som första element i respektive lista i alla tupler i listan aList
 attachHeads :: a -> a -> [([a],[a])] -> [([a],[a])]
 attachHeads h1 h2 aList = [(h1:xs,h2:ys) | (xs,ys) <- aList]
 
+----- 2c -----
 maximaBy :: Ord b => (a -> b) -> [a] -> [a]
 maximaBy valueFcn xs = [x | x <- xs, valueFcn x == maximum (map valueFcn xs)]
 
+----- 2d -----
 optAlignments :: String -> String -> [AlignmentType]
-optAlignments x [] = [(x,replicate (length x) '-')]
-optAlignments [] y = [(replicate (length y) '-', y)]
+optAlignments [] [] = [("","")]
+optAlignments (x:xs) [] = attachHeads x '-' (optAlignments xs [])
+optAlignments [] (y:ys) = attachHeads '-' y (optAlignments [] ys)
 optAlignments (x:xs) (y:ys) = concat (maximaBy (optMatchScore.head) [(attachHeads x y (optAlignments xs ys)), (attachHeads x '-' (optAlignments xs (y:ys))), (attachHeads '-' y (optAlignments (x:xs) ys))])
 
 optMatchScore :: AlignmentType -> Int
@@ -37,6 +55,7 @@ optScorer :: String -> String -> Int
 optScorer [] _ = 0
 optScorer (x:xs) (y:ys) = (matchScore x y) + (optScorer xs ys)
 
+----- 2e -----
 outputOptAlignments :: String -> String -> IO ()
 outputOptAlignments string1 string2 = do
 							putStrLn ("There are " ++ show (length aligns) ++ " optimal alignments")
@@ -52,5 +71,11 @@ printAligns (x:xs) = do
 
 spaceString :: String -> String
 spaceString s = concat [[c] ++ " " | c <- s]
+
+
+----- 3 -----
+table = [[ mcsEntry i j | j<-[0..9]] | i<-[0..9] ]
+mcsEntry :: Int -> Int -> String
+mcsEntry i j = show i ++ show j
 							
 							
