6466137c22a1b87494180f90f0b5fd573fb8a8ac
Tommy <tommy.kvant@gmail.com>
2012-11-28 15:51:24 +0100

    Uppgift 2d klar

--- StringAlign.hs
+++ StringAlign.hs
@@ -4,6 +4,8 @@ scoreMatch = 0
 scoreMismatch = -1
 scoreSpace = -1
 
+type AlignmentType = (String, String)
+
 similarityScore :: String -> String -> Int
 similarityScore x [] = (length x) * scoreSpace
 similarityScore [] y = (length y) * scoreSpace
@@ -22,3 +24,15 @@ attachHeads h1 h2 aList = [(h1:xs,h2:ys) | (xs,ys) <- aList]
 
 maximaBy :: Ord b => (a -> b) -> [a] -> [a]
 maximaBy valueFcn xs = [x | x <- xs, valueFcn x == maximum (map valueFcn xs)]
+
+optAlignments :: String -> String -> [AlignmentType]
+optAlignments x [] = [(x,replicate (length x) '-')]
+optAlignments [] y = [(replicate (length y) '-', y)]
+optAlignments (x:xs) (y:ys) = concat (maximaBy (optMatchScore.head) [(attachHeads x y (optAlignments xs ys)), (attachHeads x '-' (optAlignments xs (y:ys))), (attachHeads '-' y (optAlignments (x:xs) ys))])
+
+optMatchScore :: AlignmentType -> Int
+optMatchScore (string1, string2) = optScorer string1 string2
+
+optScorer :: String -> String -> Int
+optScorer [] _ = 0
+optScorer (x:xs) (y:ys) = (matchScore x y) + (optScorer xs ys)
