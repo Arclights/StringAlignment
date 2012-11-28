6c11f02b0c4371ffc14aec089ffc7e8c67e67b9f
Tommy <tommy.kvant@gmail.com>
2012-11-28 14:30:18 +0100

    Uppgift 2a och 2b klara

---
+++ StringAlign.hs
@@ -0,0 +1,24 @@
+import Data.Char
+
+scoreMatch = 0
+scoreMismatch = -1
+scoreSpace = -1
+
+similarityScore :: String -> String -> Int
+similarityScore x [] = (length x) * scoreSpace
+similarityScore [] y = (length y) * scoreSpace
+similarityScore (x:xs) (y:ys) = (max (similarityScore xs ys + matchScore x y).max (similarityScore xs (y:ys) + matchScore x '-')) ((similarityScore (x:xs) ys)+(matchScore '-' y))
+
+matchScore :: Char -> Char -> Int
+matchScore _ '-' = scoreSpace
+matchScore '-' _ = scoreSpace
+matchScore x y
+			|x == y = scoreMatch
+			|otherwise = scoreMismatch
+
+-- Lägger till h1 och h2 först i respektive lista i alla tupler i listan aList
+attachHeads :: a -> a -> [([a],[a])] -> [([a],[a])]
+attachHeads h1 h2 aList = [(h1:xs,h2:ys) | (xs,ys) <- aList]
+
+maximaBy :: Ord b => (a -> b) -> [a] -> [a]
+maximaBy valueFcn xs
